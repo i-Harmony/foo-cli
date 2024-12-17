19ab46de782875a549f65e6e79e4b344677516dc
i-Harmony <huimin.luo@gmx.de>
2024-12-17 10:27:15 +0100

    updates_prepare_for_a_Implementation

---
+++ inlineBreakpoint.js
@@ -0,0 +1,13 @@
+class Demo {
+    static getFoo() {
+        return "foo"; 
+    }
+
+    static getBoo() {
+        return "Boo"; 
+    }
+
+    static getCoo() {
+        return "Coo"; 
+    }
+}
